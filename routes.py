b9b9e5376e144f2e46f661b217ee8c582238696d
wtraceback <whxcer@outlook.com>
2021-03-18 22:55:33 +0800

    改进框架：将通用的方法放置到一个单独的文件中

--- routes.py
+++ routes.py
@@ -1,29 +1,6 @@
-import random
-
 from models.user import User
-
-
-# session 在服务器端实现过期功能
-session = {
-    # 'sessionid': {
-    #     'username': 'test',
-    #     'expired': '2021-03-14 16:07:08',
-    # }
-}
-
-
-def random_str():
-    """
-    用于生成一个随机的字符串，当做 sessionid
-    """
-    seed = 'abcdefghijklmnopqrstuvwxyz1234567890'
-    r = []
-    for i in range(16):
-        # random.randint(a, b) 返回随机整数 N 满足 a <= N <= b
-        random_index = random.randint(0, len(seed) - 1)
-        r.append(seed[random_index])
-
-    return ''.join(r)
+from utils import random_str, session, current_user, response_with_headers, \
+                    redirect, login_required
 
 
 def render_static_file(filename):
@@ -83,97 +60,19 @@ def route_static(request):
     return r
 
 
-def render_template(filename):
+def template(filename):
     path = 'templates/' + filename
     with open(path, 'r', encoding='utf-8') as f:
         return f.read()
 
 
-def response_with_headers(headers=None, code=200):
-    """
-    response 的响应头
-    """
-    code_description = {
-        '200': 'OK',
-        '301': 'Moved Permanently',
-        '302': 'Found',
-        '400': 'Bad Request',
-        '401': 'Unauthorized',
-        '403': 'Forbidden',
-        '404': 'Not Found',
-        '500': 'Internal Server Error',
-    }
-
-    h = {
-        'Content-Type': 'text/html; charset=UTF-8',
-    }
-
-    if headers is not None:
-        h.update(headers)
-
-    str_code = str(code)
-    header = 'HTTP/1.1 {} {}\r\n'.format(str_code, code_description.get(str_code, 'OK'))
-
-    headers_list = ['{}: {}\r\n'.format(k, v) for k, v in h.items()]
-    header += ''.join(headers_list)
-
-    return header
-
-
-def redirect(url, headers=None):
-    """
-    重定向函数
-    浏览器在收到 302 响应时，在 HTTP header 里面找 Location 字段并获取一个 url，然后自动请求新的 url
-    HTTP/1.1 302 xxx
-    Location: /
-
-    """
-    h = {
-        'Location': url,
-    }
-
-    if headers is not None:
-        h.update(headers)
-
-    # 增加 Location 字段并生成 HTTP 响应返回，302 响应没有 HTTP body 部分
-    r = response_with_headers(h, code=302) + '\r\n'
-
-    return r.encode('utf-8')
-
-
-def current_user(request):
-    """
-    返回当前访问者的用户名
-    """
-    session_id = request.cookies.get('user', '')
-    username = session.get(session_id, '【游客】')
-
-    return username
-
-
-def login_required(route_func):
-    """
-    验证登录权限的装饰器
-    """
-    def func(request):
-        username = current_user(request)
-        u = User.find_by(username=username)
-
-        if u is None:
-            return redirect('/login')
-
-        return route_func(request)
-
-    return func
-
-
 @login_required
-def route_index(request):
+def index(request):
     """
     主页的处理函数, 返回主页的响应
     """
     header = response_with_headers()
-    body = render_template('index.html')
+    body = template('index.html')
     username = current_user(request)
     body = body.replace('{{username}}', username)
     r = header + '\r\n' + body
@@ -181,7 +80,7 @@ def route_index(request):
     return r.encode('utf-8')
 
 
-def route_login(request):
+def login(request):
     """
     登录处理
     """
@@ -203,7 +102,7 @@ def route_login(request):
         result = ''
 
     header = response_with_headers()
-    body = render_template('login.html')
+    body = template('login.html')
     body = body.replace('{{result}}', result)
     body = body.replace('{{username}}', username)
     r = header + '\r\n' + body
@@ -211,7 +110,7 @@ def route_login(request):
     return r.encode('utf-8')
 
 
-def route_register(request):
+def register(request):
     """
     注册处理
     """
@@ -227,7 +126,7 @@ def route_register(request):
         result = ''
 
     header = response_with_headers()
-    body = render_template('register.html')
+    body = template('register.html')
     body = body.replace('{{result}}', result)
 
     r = header + '\r\n' + body
@@ -236,7 +135,7 @@ def route_register(request):
 
 
 route_dict = {
-    '/': route_index,
-    '/login': route_login,
-    '/register': route_register,
+    '/': index,
+    '/login': login,
+    '/register': register,
 }
---
+++ utils.py
@@ -0,0 +1,104 @@
+import random
+
+from models.user import User
+
+
+# session 在服务器端实现过期功能
+session = {
+    # 'sessionid': {
+    #     'username': 'test',
+    #     'expired': '2021-03-14 16:07:08',
+    # }
+}
+
+
+def login_required(route_func):
+    """
+    验证登录权限的装饰器
+    """
+    def func(request):
+        username = current_user(request)
+        u = User.find_by(username=username)
+
+        if u is None:
+            return redirect('/login')
+
+        return route_func(request)
+
+    return func
+
+
+def redirect(url, headers=None):
+    """
+    重定向函数
+    浏览器在收到 302 响应时，在 HTTP header 里面找 Location 字段并获取一个 url，然后自动请求新的 url
+    HTTP/1.1 302 xxx
+    Location: /
+
+    """
+    h = {
+        'Location': url,
+    }
+
+    if headers is not None:
+        h.update(headers)
+
+    # 增加 Location 字段并生成 HTTP 响应返回，302 响应没有 HTTP body 部分
+    r = response_with_headers(h, code=302) + '\r\n'
+
+    return r.encode('utf-8')
+
+
+def current_user(request):
+    """
+    返回当前访问者的用户名
+    """
+    session_id = request.cookies.get('user', '')
+    username = session.get(session_id, '【游客】')
+
+    return username
+
+
+def response_with_headers(headers=None, code=200):
+    """
+    response 的响应头
+    """
+    code_description = {
+        '200': 'OK',
+        '301': 'Moved Permanently',
+        '302': 'Found',
+        '400': 'Bad Request',
+        '401': 'Unauthorized',
+        '403': 'Forbidden',
+        '404': 'Not Found',
+        '500': 'Internal Server Error',
+    }
+
+    h = {
+        'Content-Type': 'text/html; charset=UTF-8',
+    }
+
+    if headers is not None:
+        h.update(headers)
+
+    str_code = str(code)
+    header = 'HTTP/1.1 {} {}\r\n'.format(str_code, code_description.get(str_code, 'OK'))
+
+    headers_list = ['{}: {}\r\n'.format(k, v) for k, v in h.items()]
+    header += ''.join(headers_list)
+
+    return header
+
+
+def random_str():
+    """
+    用于生成一个随机的字符串，当做 sessionid
+    """
+    seed = 'abcdefghijklmnopqrstuvwxyz1234567890'
+    r = []
+    for i in range(16):
+        # random.randint(a, b) 返回随机整数 N 满足 a <= N <= b
+        random_index = random.randint(0, len(seed) - 1)
+        r.append(seed[random_index])
+
+    return ''.join(r)
