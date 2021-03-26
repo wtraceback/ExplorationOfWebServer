61b068498bd53e42ceb0993be8fd76aad96e9a7e
wtraceback <whxcer@outlook.com>
2021-03-26 17:42:47 +0800

    为服务器添加多线程功能

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import urllib.parse
+import _thread
 
 from routes.routes_index import route_dict
 from routes.routes_auth import route_dict as auth_routes
@@ -41,16 +42,11 @@ class Request(object):
             'Cache-Control: no-cache'
         ]
         """
-        # 清空 headers
-        self.headers = {}
         lines = header
-        r = {}
         for line in lines:
             k, v = line.split(': ', 1)
             self.headers[k] = v
 
-        # 清除 cookie
-        self.cookies = {}
         self.set_cookies()
 
     def form(self):
@@ -72,9 +68,6 @@ class Request(object):
             f[k] = urllib.parse.unquote(v)
 
         return f
-
-
-request = Request()
 
 
 def error(request, code=404):
@@ -106,7 +99,7 @@ def parsed_path(path):
         return path, query
 
 
-def response_for_path(path):
+def response_for_path(path, request):
     """
     根据 path 调用相应的处理函数
     没有处理的 path 就返回 错误处理
@@ -151,6 +144,39 @@ def receive_by_request(conn):
     return req
 
 
+def process_request(connection):
+    """
+    处理每一个连接的 request
+    """
+    # 获取请求的数据
+    r = receive_by_request(connection)
+    r = r.decode('utf-8')
+    log('request is:\n {}'.format(r))
+
+    # Chrome 浏览器会发送空请求导致 split 得到空 list， 所以需要判断一下，防止程序崩溃
+    if len(r.split()) < 2:
+        connection.close()
+
+    # 创建一个新的 request 并设置
+    request = Request()
+
+    # 通过切分获取请求数据中的相关信息
+    req_line = r.split()
+    path = req_line[1]
+    request.method = req_line[0]
+    h_b_line = r.split('\r\n\r\n', 1)
+    request.set_headers(h_b_line[0].split('\r\n')[1:])
+    request.body = h_b_line[1]
+
+    # 用 response_for_path 函数来得到 path 对应的响应内容
+    response = response_for_path(path, request)
+    # response = b'HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=utf-8\r\n\r\n<h1>Hello World!</h1>'
+    connection.sendall(response)            # 用 sendall 发送给客户端
+
+    connection.close()                          # 发送完毕后, 关闭本次连接
+    print('线程关闭')
+
+
 def run(host='127.0.0.1', port=3000):
     """
     启动服务器
@@ -160,37 +186,16 @@ def run(host='127.0.0.1', port=3000):
         s.bind((host, port))        # 套接字绑定的 IP 与 端口
         print('Server start at', '{}:{}.'.format(host, port))
 
+        s.listen(5)
         # 用一个无限循环来处理请求
         while True:
-            s.listen(5)
             connection, address = s.accept()        # 当有客户端过来连接的时候, s.accept 函数就会返回 2 个值
             print('New client connection {}'.format(address))
             log('Connected by {}\nip is {}'.format(connection, address))
+            print('使用多线程处理请求', address)
 
-            # 获取请求的数据
-            r = receive_by_request(connection)
-            r = r.decode('utf-8')
-            log('request is:\n {}'.format(r))
-
-            # Chrome 浏览器会发送空请求导致 split 得到空 list， 所以需要判断一下，防止程序崩溃
-            if len(r.split()) < 2:
-                continue
-
-            # 通过切分获取请求数据中的相关信息
-
-            req_line = r.split()
-            path = req_line[1]
-            request.method = req_line[0]
-            h_b_line = r.split('\r\n\r\n', 1)
-            request.set_headers(h_b_line[0].split('\r\n')[1:])
-            request.body = h_b_line[1]
-
-            # 用 response_for_path 函数来得到 path 对应的响应内容
-            response = response_for_path(path)
-            # response = b'HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=utf-8\r\n\r\n<h1>Hello World!</h1>'
-            connection.sendall(response)            # 用 sendall 发送给客户端
-
-            connection.close()                          # 发送完毕后, 关闭本次连接
+            # 开一个新的线程来处理请求, 第二个参数是传给新函数的参数列表, 必须是 tuple
+            _thread.start_new_thread(process_request, (connection,))
 
 
 def main():
